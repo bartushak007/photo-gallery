f8722eed5a5e14dac24316909cd281185aba916d
rmnbrtshk <rmnbrtshk@gmail.com>
2019-03-27 17:30:15 +0200

    Updates

---
+++ precache-manifest.7704bffb19d3bbc190217ed3c7184d6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "723a16131cc7627d3256",
+    "url": "/cards_gallery/static/js/runtime~main.4132761c.js"
+  },
+  {
+    "revision": "cc06680754675c0a3058",
+    "url": "/cards_gallery/static/js/main.d840f8d3.chunk.js"
+  },
+  {
+    "revision": "f9e82c19e11d1ace00fc",
+    "url": "/cards_gallery/static/js/2.a8b79bfb.chunk.js"
+  },
+  {
+    "revision": "cc06680754675c0a3058",
+    "url": "/cards_gallery/static/css/main.8802dd37.chunk.css"
+  },
+  {
+    "revision": "b8d77093935e25e8f7f1ad5f79195088",
+    "url": "/cards_gallery/index.html"
+  }
+];
