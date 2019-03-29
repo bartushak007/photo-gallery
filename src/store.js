60ae24eb64c403994ffbe3200068f7be985caad2
rmnbrtshk <rmnbrtshk@gmail.com>
2019-03-29 19:26:30 +0200

    remove redux devtool line code

--- src/store.js
+++ src/store.js
@@ -8,10 +8,7 @@ const middleware = [thunk];
 const store = createStore(
   rootreducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(...middleware)
 );
 
 export default store;
